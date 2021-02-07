2f9fe60a6ef52e10a8438a5884262eaa140276a9
pourquoi <mathias.dusautoy@gmail.com>
2021-02-07 16:45:28 +0100

    up

--- chaperons-backend/bin/up.sh
+++ chaperons-backend/bin/up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 php bin/console doctrine:schema:create -qn
+php bin/console doctrine:schema:update -qn --force
 
 php bin/console doctrine:database:import -qn src/App/get_total_nurseries.sql
 php bin/console doctrine:database:import -qn src/App/update_close_nurseries.sql
